f6e56cd00256d1cce6ae9969a65ce0ce51ab1a1b
quondem <86314992+quondem@users.noreply.github.com>
2024-07-19 14:14:47 +0300

    title & menu position

--- components/Templates/Main/Fines/Fines.vue
+++ components/Templates/Main/Fines/Fines.vue
@@ -1,6 +1,6 @@
 <template>
 	<AppSection class="fines section_without-background">
-		<AppH1> Проверьте штрафы {{ titleMap[route.query.type] }} и зарегистрируйтесь в 1 клик </AppH1>
+		<AppH1 class="fines__title"> Проверьте штрафы {{ titleMap[route.query.type] }} и зарегистрируйтесь в 1 клик </AppH1>
 		<form
 			class="fines__form"
 			@click.prevent
